a7081570f3168c06862e27235c3be403488a9437
visualstudio <visualstudio@122-PC20>
2019-02-01 19:17:15 -0500

    C0723560_WEEK04

--- CSD3354-week 04/Program.cs
+++ CSD3354-week 04/Program.cs
@@ -6,10 +6,42 @@ using System.Threading.Tasks;
 
 namespace CSD3354_week_04
 {
-    class Program
+   
+class Program
     {
         static void Main(string[] args)
         {
+
+
         }
     }
+    class village
+    {
+        public bool isAstrildeHere;
+        public village nextvillage;
+        public village prevvillage;
+        public String villagename;
+
+
+    }
+
+
+    class countryside
+    {
+        village maple;
+        village toronto;
+        village ajax;
+
+        public void run()
+        {
+            maple = new village();
+            maple.villagename = "toronto";
+            maple.nextvillage = toronto;
+
+        }
+
+    }
+
+
+
 }
